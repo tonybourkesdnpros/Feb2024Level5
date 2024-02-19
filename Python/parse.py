d4aa0bb67649a633ddbb33776c744e67328f840d
arista <atd-help@arista.com>
2024-02-19 02:41:33 +0000

    First commit

---
+++ Python/parse.py
@@ -0,0 +1,10 @@
+import json
+
+file = open('results.json', 'r')
+
+results = json.load(file)
+
+for interface in results['result'][0]['interfaces']:
+    print(interface+':', results['result'][0]['interfaces'][interface]['interfaceAddress']['ipAddr']['address'])
+
+
