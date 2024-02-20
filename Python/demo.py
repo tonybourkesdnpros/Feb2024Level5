807a4c6923f7ebc524d46a57cf5ea6f17039ab1f
arista <atd-help@arista.com>
2024-02-20 04:07:24 +0000

    Added demo

---
+++ Python/demo.py
@@ -0,0 +1,40 @@
+# Variables
+
+## Simple Variables
+
+### Integers
+
+i = 1
+n = 5
+
+m = i + n
+
+### Strings
+
+greeting = "Hello, world!"
+
+first_word = "Hello, "
+second_word = "world!"
+
+full_greeting = first_word + second_word
+
+
+### Boolean
+
+World_is_flat = True
+
+
+### Conditionals
+
+# if World_is_flat == False:
+#     print("NASA was right!")
+# if World_is_flat == True:
+#     print("I fell off the edge")
+
+### Complex Variables (lists, dictionaries)
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+i = 1
+for item in planets:
+    print("The", i, "planet from the sun is:", item)
+    i = i + 1 
