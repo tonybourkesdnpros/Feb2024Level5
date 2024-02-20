7145ce18ef52670fff5b796e2e51f6ba8c21f1c0
arista <atd-help@arista.com>
2024-02-20 04:46:59 +0000

    end of day 2

--- Python/gen_config.py
+++ Python/gen_config.py
@@ -1,12 +1,16 @@
 import yaml
 
+mtu = 9214
 yaml_file = open('devices.yml', 'r')
 
 devices = yaml.safe_load(yaml_file)
 
 for item in devices:
     print("Switch", item)
-    for interface in devices[item]['interfaces']:
-        ip = devices[item]['interfaces'][interface]['ipv4']
-        print("interface", interface)
+    for item2 in devices[item]['interfaces']:
+        ip = devices[item]['interfaces'][item2]['ipv4']
+        print("interface", item2)
+        if "thernet" in item2:
+            print("  no switchport")
+            print("  mtu", mtu)
         print("  ip address", ip)
