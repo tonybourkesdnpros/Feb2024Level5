7fd657d7465125d52122110ffeefa1ccdae1b808
arista <atd-help@arista.com>
2024-02-21 03:14:39 +0000

    Some more python

---
+++ Python/pyeapi/test.py
@@ -0,0 +1,11 @@
+import pyeapi
+import json
+
+
+switches = ['leaf1-DC1', 'leaf2-DC1', 'leaf3-DC1', 'leaf4-DC1']
+
+for switch in switches:
+    node = pyeapi.connect_to(switch)
+    mac_table = node.enable('show mac address-table')
+    for mac in mac_table[0]['result']['unicastTable']['tableEntries']:
+        print(switch+':', mac['macAddress'])
