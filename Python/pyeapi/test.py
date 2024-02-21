1910d314355b5000e1d40e6fb1e6f7377ecbc623
arista <atd-help@arista.com>
2024-02-21 22:14:32 +0000

    Start of Day 4

--- Python/pyeapi/test.py
+++ Python/pyeapi/test.py
@@ -7,5 +7,6 @@ switches = ['leaf1-DC1', 'leaf2-DC1', 'leaf3-DC1', 'leaf4-DC1']
 for switch in switches:
     node = pyeapi.connect_to(switch)
     mac_table = node.enable('show mac address-table')
-    for mac in mac_table[0]['result']['unicastTable']['tableEntries']:
-        print(switch+':', mac['macAddress'])
+    print("The switch", switch, "knows ", len(mac_table[0]['result']['unicastTable']['tableEntries']), "MAC addresses")
+    # for mac in mac_table[0]['result']['unicastTable']['tableEntries']:
+    #     print(switch+':', mac['macAddress'])
